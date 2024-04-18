9e0f9a1a4f506da38925a093ba65695a27216981
MirtalibEmirli <azad.sdk.85@icloud.com>
2024-04-19 02:10:48 +0400

    primitiv full version

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -9,16 +9,233 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Calculator
+namespace Calculator;
+
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public int x = 0;
+    public int y = 0;
+    string cem;
+    bool clear;
+    char op;
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void beraber_Click(object sender, RoutedEventArgs e)
+    {
+
+        y = Convert.ToInt32(label1.Content);
+
+        switch (op)
         {
-            InitializeComponent();
+
+            case '+':
+                cem = (x + y).ToString();
+
+                break;
+            case '*':
+
+                cem = (x * y).ToString();
+
+
+
+                break;
+
+            case '-':
+                cem = (x - y).ToString();
+
+
+
+                break;
+            case '/':
+
+                cem = (x / y).ToString();
+
+                break;
+
+            default:
+                break;
         }
+
+
+
+
+
+        label1.Content = cem;
+    }
+
+    private void Topla_Click(object sender, RoutedEventArgs e)
+    {
+        op = '+';
+        clear = true;
+        x = Convert.ToInt32(label1.Content);
+        label1.Content = "0";
+
+    }
+
+    private void Cix_Click(object sender, RoutedEventArgs e)
+    {
+        op = '-';
+
+        clear = true;
+        x = Convert.ToInt32(label1.Content);
+        label1.Content = "0";
+    }
+
+    private void Vur_Click(object sender, RoutedEventArgs e)
+    {
+        op = '*';
+
+        clear = true;
+        x = Convert.ToInt32(label1.Content);
+        label1.Content = "0";
+    }
+
+    private void bol_Click(object sender, RoutedEventArgs e)
+    {
+        op = '/';
+
+        clear = true;
+        x = Convert.ToInt32(label1.Content);
+        label1.Content = "0";
+    }
+
+
+
+   
+
+    private void doqquz_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "9";
+    }
+
+    private void yeddi_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "7";
+    }
+
+    private void sifir_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear) label1.Content = "";
+
+        label1.Content += "0";
+    }
+
+    private void sekkiz_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "8";
+    }
+
+    private void dord_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "4";
+    }
+
+    private void bes_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "5";
+    }
+
+    private void alti_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "6";
+    }
+
+    private void uc_Click_1(object sender, RoutedEventArgs e)
+    {
+
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "3";
+    }
+
+    private void iki_Click_1(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0")
+        {
+            label1.Content = "";
+        }
+        label1.Content += "2";
+    }
+
+    private void bir_Click(object sender, RoutedEventArgs e)
+    {
+        if (clear)
+        {
+            label1.Content = "";
+            clear = false;
+        }
+        if (label1.Content == "0") label1.Content = "";
+
+        label1.Content += "1";
     }
 }
